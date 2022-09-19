89e4fc7605cd55ac7626e778903d6bdad1421c83
Septiana Yoga <septianayoga30@gmail.com>
2022-09-19 10:28:33 +0700

    initializing image assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,33 @@
+import audiLineup from "./lineup/audi.png";
+import ferrariLineup from "./lineup/ferrari.png";
+import lamborghiniLineup from "./lineup/lamborghini.png";
+import mercedesLineup from "./lineup/mercedes.png";
+import porscheLineup from "./lineup/porsche.png";
+import audiList from "./list/audi.png";
+import ferrariList from "./list/ferrari.png";
+import lamborghiniList from "./list/lamborghini.png";
+import mercedesList from "./list/mercedes.png";
+import porscheList from "./list/porsche.png";
+import blog1 from "./blog-1.png";
+import blog2 from "./blog-2.png";
+import blog3 from "./blog-3.png";
 import Hero from "./hero.png";
+import specialOrder from "./special-order.png";
 
-export { Hero };
+export {
+  Hero,
+  blog1,
+  blog2,
+  blog3,
+  specialOrder,
+  audiLineup,
+  ferrariLineup,
+  lamborghiniLineup,
+  mercedesLineup,
+  porscheLineup,
+  audiList,
+  ferrariList,
+  lamborghiniList,
+  mercedesList,
+  porscheList,
+};
