08df5da8b4f3673a7aa70d864cf5a3f9d424adcc
Septiana Yoga <septianayoga30@gmail.com>
2022-09-19 14:05:13 +0700

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Navbar } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
+      <Navbar />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <div className="left">AUTOTIME</div>
+      <div className="middle">
+        <ul>
+          <li>Service</li>
+          <li>Cars</li>
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div className="right">
+        <button>Get a Car</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
