bd0f2a8d9c29cff00dbd4df5f858820a92607555
Septiana Yoga <septianayoga30@gmail.com>
2022-09-22 14:30:13 +0700

    adding minor

--- src/components/lineupCar/LineupCar.js
+++ src/components/lineupCar/LineupCar.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "./Lineup.css";
 
 const LineupCar = () => {
-  return <div>LineupCar</div>;
+  return (
+    <div className="lineup-container">
+      <h2>Our Best Lineup</h2>
+      <div className="lineup-content">
+        <div className="top">
+          <div className="left">{/* image goes here */}</div>
+          <div className="right">
+            <h3 className="lineup-car-name">Ferrari</h3>
+            <p className="lineup-car-type">LaFerrari</p>
+            <p className="lineup-car-desc">
+              LaFerrari, project name F150[4] is a limited production mid-engine
+              mild hybrid sports car built by Italian automotive manufacturer
+              Ferrari
+            </p>
+            <button className="lineup-see-more">See More</button>
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default LineupCar;
