a24ee98a891bd42b39a400ffc42838603e5eaeca
Septiana Yoga <septianayoga30@gmail.com>
2022-09-24 14:03:27 +0700

    structuring blog code

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="blog-container">
+      <div className="mini-blog">
+        {/* <img src="" alt="" /> */}
+        <h3>Check Your Engine Now!</h3>
+        <button>Read More</button>
+        <hr />
+      </div>
+      <div className="mini-blog">
+        {/* <img src="" alt="" /> */}
+        <h3>I'm Tired With Tires</h3>
+        <button>Read More</button>
+        <hr />
+      </div>
+      <div className="full-blog">
+        {/* <img src="" alt="" /> */}
+        <h3>Basic stuff to adjust your belt</h3>
+        <button>Read More</button>
+        <hr />
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
