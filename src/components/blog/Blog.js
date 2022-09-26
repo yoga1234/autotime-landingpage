955de75e53eefdb14263bc0bc5a1cf7b5b1b31a2
Septiana Yoga <septianayoga30@gmail.com>
2022-09-26 11:22:43 +0700

    structuring blog layout

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,25 +1,31 @@
 import React from "react";
+import "./Blog.css";
+
+import { blog1, blog2, blog3 } from "../../assets";
 
 const Blog = () => {
   return (
     <div className="blog-container">
-      <div className="mini-blog">
-        {/* <img src="" alt="" /> */}
-        <h3>Check Your Engine Now!</h3>
-        <button>Read More</button>
-        <hr />
-      </div>
-      <div className="mini-blog">
-        {/* <img src="" alt="" /> */}
-        <h3>I'm Tired With Tires</h3>
-        <button>Read More</button>
-        <hr />
-      </div>
-      <div className="full-blog">
-        {/* <img src="" alt="" /> */}
-        <h3>Basic stuff to adjust your belt</h3>
-        <button>Read More</button>
-        <hr />
+      <h2 className="blog-title">Blog</h2>
+      <div className="blog-content">
+        <div className="mini-blog">
+          <img src={blog1} alt="" />
+          <h3>Check Your Engine Now!</h3>
+          <button>Read More</button>
+          <hr />
+        </div>
+        <div className="mini-blog">
+          <img src={blog2} alt="" />
+          <h3>I'm Tired With Tires</h3>
+          <button>Read More</button>
+          <hr />
+        </div>
+        <div className="full-blog">
+          <img src={blog3} alt="" />
+          <h3>Basic stuff to adjust your belt</h3>
+          <button>Read More</button>
+          <hr />
+        </div>
       </div>
     </div>
   );
