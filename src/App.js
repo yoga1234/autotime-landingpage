ef0a3cd179b6ca7097431832f9e3ca8eb397867f
Septiana Yoga <septianayoga30@gmail.com>
2022-09-29 13:21:19 +0700

    adding logic for choosing car list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
+import data from "./data.json";
 import {
   Blog,
   Footer,
@@ -11,6 +15,17 @@ import {
 } from "./components";
 
 const App = () => {
+  const [defaultCarList, setDefaultCarList] = useState("ferrari");
+  const [activeCarList, setActiveCarList] = useState({});
+  // handling lineup change
+
+  useEffect(() => {
+    for (var key in data.carList) {
+      if (key === defaultCarList) {
+        setActiveCarList(data.carList[key]);
+      }
+    }
+  }, []);
   return (
     <div className="App">
       <Navbar />
